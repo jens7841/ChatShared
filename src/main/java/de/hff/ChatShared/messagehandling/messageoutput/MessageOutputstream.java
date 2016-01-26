9c676122c23ecc05cd6d001aacf9a8e9d6ae5e74
jens7841 <jens7841@web.de>
2016-01-26 15:00:10 +0100

    Fix

--- src/main/java/de/hff/ChatShared/messagehandling/messageoutput/MessageOutputstream.java
+++ src/main/java/de/hff/ChatShared/messagehandling/messageoutput/MessageOutputstream.java
@@ -1,5 +1,6 @@
 package de.hff.ChatShared.messagehandling.messageoutput;
 
+import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
@@ -9,12 +10,14 @@ import de.hff.ChatShared.messagehandling.Message;
 
 public class MessageOutputstream extends FilterOutputStream {
 
+	private DataOutputStream output;
+
 	public MessageOutputstream(OutputStream out) {
 		super(out);
+		output = new DataOutputStream(new BufferedOutputStream(out));
 	}
 
 	public void writeMessage(Message message) throws IOException {
-		DataOutputStream output = new DataOutputStream(out);
 		output.write(message.getType().getTypeNumber());
 		output.writeInt(message.getBytes().length);
 		output.write(message.getBytes());
